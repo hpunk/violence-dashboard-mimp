42d3b9f722822c6fc8d6763697025d794f85c064
hpunk <gustavoalzamora1997@gmail.com>
2022-04-19 17:11:38 -0300

    videos added instead of images for clustering additional info

--- src/views/Clustering/AdditionalInfo.js
+++ src/views/Clustering/AdditionalInfo.js
@@ -29,18 +29,18 @@ const {Option} = Select;
 
 function AdditionalInfo({algorithm}){
     const blockContent = algorithmDescription[algorithm];
-    const images = {
-        '0' : kmedoidsImage,
-        '1' : dbscanImage,
-        '2' : linkageImage,
+    const videos = {
+        '0' : <iframe width="1120" height="630" src="https://www.youtube.com/embed/1Rp0hsMAfFY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+        '1' : <iframe width="1120" height="630" src="https://www.youtube.com/embed/YX8OBf80OVQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+        '2' : <iframe width="1120" height="630" src="https://www.youtube.com/embed/4fgKYQQ0kW8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
     };
     return (
         <div>
           <AlgorithmAdditionalInfo>
             <Text strong type="primary">{`Algoritmo ${clusteringAlgorithmsNames[algorithm]}:`}</Text>
             <div class="imgviewer" style={{ marginLeft : "45px"}}>
-                <div style={{fontWeight: "bold", border: "2px solid black"}}>Visualizar ejemplo</div>
-                <span class="imgviewercontent"><img src={images[algorithm]} alt="algorithm" width="100%"/></span>
+                <div style={{fontWeight: "bold", border: "2px solid black", padding: "2px"}}>Ver vídeo</div>
+                <span class="imgviewercontent">{videos[algorithm]}</span>
             </div>
             <Paragraph>
               <pre>{blockContent}</pre>
