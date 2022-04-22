e7e6445b8559ae2d373721b1020207c890e50f78
hpunk <gustavoalzamora1997@gmail.com>
2022-04-22 14:13:48 -0300

    tutorials added to home page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import {Typografy, Typography} from 'antd';
+import {
+  HomeContainer,
+  ModulesAreaContainer,
+  TitleContainer,
+  ModuleContainer,
+} from './Home.styles.js';
+import Popup from 'reactjs-popup';
+import impactImage from './images/impacto_foto.png'
+import evolutionImage from './images/evolution_foto.png'
+import clusteringImage from './images/clustering_foto.png'
 
-const {Title} = Typography;
+const {Title, Paragraph} = Typography;
 
 const Home = () => {
   return (
-  <div>
-    <Title>Herramienta de analítica visual para datos sobre violencia publicados por el MIMP</Title>
-    <h2>Tesista: Gustavo André Alzamora Vargas</h2>
-    <br/>
-    <h3>El primer módulo trata de ayudar a visualizar el impacto que puede existir en el registro de los casos de violencia posterior a la realización de acciones preventivas.</h3>
-    <br/>
-    <h3>El segundo módulo puede ser usado para visualizar cómo es que los casos de violencia reportados varían a través del tiempo en los diferentes departamentos del Perú</h3>
-    <br/>
-    <h3>El tercer módulo permite llevar a cabo la agrupación de casos de violencia basado en las características presentes en cada uno de los casos reportados</h3>
-  </div>
+    <HomeContainer>
+      <TitleContainer>
+        <Title>Herramienta de analítica visual para datos sobre violencia publicados por el MIMP</Title>
+        <h2>Tesista: Gustavo André Alzamora Vargas</h2>
+        <h3>La presente herramienta usa datos de casos de violencia y acciones preventivas publicados por el MIMP para los años 2017, 2018, 2019, 2020</h3>
+      </TitleContainer>
+      <ModulesAreaContainer>
+        <ModuleContainer>
+          <img src={impactImage} width={"100%"} alt="impact_image" />
+          <Paragraph><pre>El primer módulo "Impacto Acciones Preventivas", trata de ayudar a visualizar el impacto que puede existir por parte de las acciones preventivas en los casos de violencia posteriormente registrados</pre></Paragraph>
+          <Popup modal nested trigger={<div style={{textAlign:"center"}}><button> Ver tutorial</button></div>}>
+            <iframe width="1120" height="630" src="https://www.youtube.com/embed/jH9VR5mhcW0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Popup>
+        </ModuleContainer>
+        <ModuleContainer>
+          <img src={evolutionImage} width={"100%"} alt="evolution_image" />
+          <Paragraph><pre>El segundo módulo "Evolución de la violencia", puede ser usado para visualizar cómo es que los casos de violencia reportados varían a través del tiempo en los diferentes departamentos del Perú</pre></Paragraph>
+          <Popup modal nested trigger={<div style={{textAlign:"center"}}><button> Ver tutorial</button></div>}>
+            <iframe width="1120" height="630" src="https://www.youtube.com/embed/jH9VR5mhcW0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Popup>
+        </ModuleContainer>
+        <ModuleContainer>
+          <img src={clusteringImage} width={"100%"} alt="clustering_image" />
+          <Paragraph><pre>El tercer módulo "Clustering", permite llevar a cabo la identificación de grupos de casos de violencia basándose en qué tan parecidas son las características presentes en los casos registrados</pre></Paragraph>
+          <Popup modal nested trigger={<div style={{textAlign:"center"}}><button> Ver tutorial</button></div>}>
+            <iframe width="1120" height="630" src="https://www.youtube.com/embed/jH9VR5mhcW0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Popup>
+        </ModuleContainer>
+      </ModulesAreaContainer>
+    </HomeContainer>
   )
 }
 
---
+++ src/views/Home/Home.styles.js
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const TitleContainer = styled.div`
+    width: 100%;
+    height: 15%;
+`
+
+export const ModulesAreaContainer = styled.div`
+    width: 100%;
+    height: 55%;
+    display: flex;
+    background-color: white;
+    justify-content: space-around;
+`
+
+export const HomeContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    flex: none;
+`
+
+export const ModuleContainer = styled.div`
+    padding-top: 3%;
+    padding-left: 0.5%;
+    padding-right:0.5%;
+    border-style: groove;
+    border-size: 2px;
+    width: 33%;
+    height: 100%;
+`
