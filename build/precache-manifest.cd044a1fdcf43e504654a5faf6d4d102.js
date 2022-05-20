5a4395230e4af21cd138086198fa4fdc5fc57964
hpunk <gustavoalzamora1997@gmail.com>
2022-05-20 20:09:00 -0300

    build added

---
+++ build/precache-manifest.cd044a1fdcf43e504654a5faf6d4d102.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa118fd659de0446329dff0b74dcb79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723b639d52f7b891153a",
+    "url": "/static/css/2.60ba142b.chunk.css"
+  },
+  {
+    "revision": "f4c40f6390b454f575f9",
+    "url": "/static/css/main.1fc129db.chunk.css"
+  },
+  {
+    "revision": "723b639d52f7b891153a",
+    "url": "/static/js/2.f6e29c83.chunk.js"
+  },
+  {
+    "revision": "3bc1e236684516ed23e6d2e6bc3bdc1b",
+    "url": "/static/js/2.f6e29c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c40f6390b454f575f9",
+    "url": "/static/js/main.4379efe4.chunk.js"
+  },
+  {
+    "revision": "7b83e5f3f11134f4a122",
+    "url": "/static/js/runtime-main.c9d7978f.js"
+  },
+  {
+    "revision": "e417d458233b9510c85f0c2fea315bcd",
+    "url": "/static/media/average_linkage_steps.drawio.e417d458.png"
+  },
+  {
+    "revision": "816e8ea209109f8e2ca96701b61cd82a",
+    "url": "/static/media/clustering_foto.816e8ea2.png"
+  },
+  {
+    "revision": "40e5431b5cfa895d1c2ad87c6299c023",
+    "url": "/static/media/dbscan-steps.drawio.40e5431b.png"
+  },
+  {
+    "revision": "c3d8ad85d02c8af886c8351aacce577c",
+    "url": "/static/media/evolution_foto.c3d8ad85.png"
+  },
+  {
+    "revision": "072a7f9fe2b6b1bb53da6e7826a9027f",
+    "url": "/static/media/impacto_foto.072a7f9f.png"
+  },
+  {
+    "revision": "d0702b91512998ded4968fa06c4ec233",
+    "url": "/static/media/kmedoids-steps.drawio.d0702b91.png"
+  }
+]);
