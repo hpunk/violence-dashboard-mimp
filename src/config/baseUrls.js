aec4f0e0da8a8fea55804d9c67989bde2add048e
hpunk <gustavoalzamora1997@gmail.com>
2022-05-29 23:05:36 -0300

    baseurls fixed

--- src/config/baseUrls.js
+++ src/config/baseUrls.js
@@ -1,2 +1,2 @@
-export const app_evolution_url = "evolution.proyectotesisgaav.cloudns.ph/";
-export const clustering_url = "clustering.proyectotesisgaav.cloudns.ph/";
+export const app_evolution_url = "visualization.proyectotesisgaav.cloudns.ph";
+export const clustering_url = "clustering.proyectotesisgaav.cloudns.ph";
